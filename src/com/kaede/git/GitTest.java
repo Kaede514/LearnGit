ee9a9a9a0b01c991283a94aff0590bc1213bad2c
Kaede <Kaede@xx.com>
2022-07-15 12:59:01 +0800

    first commit

---
+++ src/com/kaede/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.kaede.git;
+
+/**
+ * @author kaede
+ * @create 2022-07-15 12:54
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
