edb07c63db155361cac82431a508039f40eec572
kaede <xx@qq.com>
2022-10-09 20:09:17 +0800

    first commit

---
+++ src/main/java/com/kaede/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.kaede.git;
+
+/**
+ * @author kaede
+ * @create 2022-10-09
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
---
+++ src/main/java/com/kaede/git/SecondCommit.java
@@ -0,0 +1,8 @@
+package com.kaede.git;
+
+/**
+ * @author kaede
+ * @create 2022-10-09
+ */
+public class SecondCommit {
+}
